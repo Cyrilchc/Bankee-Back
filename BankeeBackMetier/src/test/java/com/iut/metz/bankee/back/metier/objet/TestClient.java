59a745a457891a8f97598cee915d8e5094046d4b
LoicNoelPro <loic.noel9757@gmail.com>
2019-04-25 17:18:28 +0200

    [merge] origin/master (correction pom)

--- BankeeBackMetier/src/test/java/com/iut/metz/bankee/back/metier/objet/TestClient.java
+++ BankeeBackMetier/src/test/java/com/iut/metz/bankee/back/metier/objet/TestClient.java
@@ -1,46 +1,45 @@
-package com.iut.metz.bankee.back.metier.objet;
-
-import metier.exception.ClientException;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-/**
- *
- * @author Lo�c NOEL
- */
-public class TestClient {
-    
-    Client client;
-
-    @Before
-    public void setUp() {
-        
-        client = new Client("CL1", "4 rue des peupliers", "MR JEAN DUPONT");
-    }
-
-    @Test
-    public void testInitialisationListComptes() {
-        int expected = 0;
-        int out = client.nbComptes();
-        Assert.assertEquals(expected, out);
-    }
-    
-    @Test(expected = ClientException.class)
-    public void testAjoutCompteNull() throws ClientException {
-        Compte compte = null;
-        boolean out = client.ajouterCompte(compte);
-    }
-    
-    @Test
-    public void testAjoutCompteAvecDecouvert() {
-        // TODO
-    }
-    
-    @Test
-    public void testAjoutCompteSansDecouvert() {
-        // TODO
-    }
-    
-    
-}
+//package com.iut.metz.bankee.back.metier.objet;
+//
+//import org.junit.Assert;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+///**
+// *
+// * @author Lo�c NOEL
+// */
+//public class TestClient {
+//    
+//    Client client;
+//
+//    @Before
+//    public void setUp() {
+//        
+//        client = new Client("CL1", "4 rue des peupliers", "MR JEAN DUPONT");
+//    }
+//
+//    @Test
+//    public void testInitialisationListComptes() {
+//        int expected = 0;
+//        int out = client.nbComptes();
+//        Assert.assertEquals(expected, out);
+//    }
+//    
+//    @Test(expected = ClientException.class)
+//    public void testAjoutCompteNull() throws ClientException {
+//        Compte compte = null;
+//        boolean out = client.ajouterCompte(compte);
+//    }
+//    
+//    @Test
+//    public void testAjoutCompteAvecDecouvert() {
+//        // TODO
+//    }
+//    
+//    @Test
+//    public void testAjoutCompteSansDecouvert() {
+//        // TODO
+//    }
+//    
+//    
+//}
