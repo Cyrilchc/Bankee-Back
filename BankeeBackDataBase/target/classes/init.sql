201c27454a0fdbb00959980815baf92b2e87c223
CharlesLgn <charles.ligony3@etu.univ-lorraine.fr>
2019-04-26 06:24:10 +0200

    devops

--- BankeeBackDataBase/target/classes/init.sql
+++ BankeeBackDataBase/target/classes/init.sql
@@ -1,1 +1,26 @@
-CREATE DATABASE bankee;
+-- table client
+CREATE TABLE client
+(
+    id            SERIAL PRIMARY KEY,
+    numero_client VARCHAR(255) NOT NULL,
+    adresse       VARCHAR(255) NOT NULL,
+    nom           VARCHAR(255) NOT NULL
+);
+
+-- table compte
+CREATE TABLE compte
+(
+    id                 SERIAL PRIMARY KEY,
+    numero_compte      VARCHAR(255) NOT NULL,
+    solde              DECIMAL DEFAULT 0,
+    decouvert_autorise DECIMAL DEFAULT NULL
+);
+
+-- table appartient
+CREATE TABLE appartient
+(
+    id_compte INTEGER NOT NULL,
+    id_client INTEGER NOT NULL
+);
+-- les deux clé sont primaire car un compte peut appartenir à plusieur personne (ex. compte pour un colocation)
+ALTER TABLE appartient ADD CONSTRAINT appartient_pkey PRIMARY KEY (id_compte,id_client);
