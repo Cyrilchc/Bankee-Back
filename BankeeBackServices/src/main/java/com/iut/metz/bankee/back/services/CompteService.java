4f7cc1489eacd716a5fbed06a77c0d92dd2a1bb9
CharlesLgn <charles.ligony3@etu.univ-lorraine.fr>
2019-04-28 19:50:21 +0200

    vfhjhsdfjlhgdfiobsd;,bsdfjhgdsnsdkjgsdkjhh sdjksdhgdsiuh sdkbjk

--- BankeeBackServices/src/main/java/com/iut/metz/bankee/back/services/CompteService.java
+++ BankeeBackServices/src/main/java/com/iut/metz/bankee/back/services/CompteService.java
@@ -1,33 +1,34 @@
 package com.iut.metz.bankee.back.services;
 
-import static com.iut.metz.bankee.back.services.util.CustomHeader.NUMERO_COMPTE;
-
-import javax.ws.rs.*;
-import javax.ws.rs.core.*;
-
 import com.iut.metz.bankee.back.metier.manager.CompteManager;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static com.iut.metz.bankee.back.services.util.CustomHeader.NUMERO_COMPTE;
 
 /**
  * service ne s'occupant que d'un compte
  */
+
 @Path("compte")
 public class CompteService {
 
   /**
    * retourne le compte demandé en fonction de son numéro de compte
    */
+
   @GET
   @Produces({ MediaType.APPLICATION_JSON })
-  public Response getClient() {
-    return Response
-            .status(200)
-            .header("Access-Control-Allow-Origin", "*")
-            .header("Access-Control-Allow-Credentials", "true")
-            .header("Access-Control-Allow-Headers",
-                    "origin, content-type, accept, authorization")
-            .header("Access-Control-Allow-Methods",
-                    "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-            .entity("")
-            .build();
+  public Response getClient(@Context HttpHeaders headers) {
+    return new getDataService().getData(headers,
+            NUMERO_COMPTE,
+            num -> CompteManager.getInstance().getCompteByNumCompte(num));
   }
 }
--- BankeeBackServices/src/main/java/com/iut/metz/bankee/back/services/util/ServiceUtils.java
+++ BankeeBackServices/src/main/java/com/iut/metz/bankee/back/services/util/ServiceUtils.java
@@ -13,13 +13,7 @@ public class ServiceUtils {
   public static Response getResponse(Object object) {
     return Response
             .status(200)
-            .header("Access-Control-Allow-Origin", "http://localhost:4200")
-            .header("Access-Control-Allow-Credentials", "true")
-            .header("Access-Control-Allow-Headers",
-                    "origin, content-type, accept, authorization")
-            .header("Access-Control-Allow-Methods",
-                    "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-            .entity("")
+            .entity(object)
             .build();
   }
 
