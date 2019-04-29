1443da00bad099a30130913a1697ddbf6f1c3bc9
charles ligony <c-ligony@efluid.fr>
2019-04-29 08:54:17 +0200

    bug

--- BankeeBackServices/src/main/java/com/iut/metz/bankee/back/services/util/CustomHeader.java
+++ BankeeBackServices/src/main/java/com/iut/metz/bankee/back/services/util/CustomHeader.java
@@ -1,6 +1,10 @@
 package com.iut.metz.bankee.back.services.util;
 
 public class CustomHeader {
+  private CustomHeader() {
+    //empty constructor
+  }
+
   public static final String NUMERO_COMPTE = "x-numero-compte";
   public static final String NUMERO_CLIENT = "x-numero-client";
 }
--- BankeeBackServices/src/main/java/com/iut/metz/bankee/back/services/util/ServiceConstante.java
+++ BankeeBackServices/src/main/java/com/iut/metz/bankee/back/services/util/ServiceConstante.java
@@ -1,6 +1,10 @@
 package com.iut.metz.bankee.back.services.util;
 
 public class ServiceConstante {
+  private ServiceConstante() {
+    //empty constructor
+  }
+
   public static final String NUMERO_NON_VALIDE = "Veuillez fournir un numero valide";
   public static final String NON_AUTORISE = "non autorisé";
 }
